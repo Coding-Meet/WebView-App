1f33cfeba103198c532808a7b4a552f449b9d75c
coding meet <codingmeet26@gmail.com>
2023-09-26 17:07:34 +0530

    WebView Inside Download Manager

--- app/src/main/java/com/coding/meet/webviewtoapp/MainActivity.kt
+++ app/src/main/java/com/coding/meet/webviewtoapp/MainActivity.kt
@@ -1,28 +1,48 @@
 package com.coding.meet.webviewtoapp
 
 import android.app.Dialog
+import android.app.DownloadManager
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.os.Looper
 import android.provider.Settings
+import android.util.Log
 import android.view.KeyEvent
+import android.webkit.CookieManager
+import android.webkit.URLUtil
 import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.coding.meet.webviewtoapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
-
     private var webUrl = "https://github.com/Coding-Meet"
-
+//    private var webUrl = "https://sample-videos.com/download-sample-jpg-image.php"
+    private val multiplePermissionId = 14
+    private val multiplePermissionNameList = if (Build.VERSION.SDK_INT >= 33) {
+        arrayListOf()
+    } else {
+        arrayListOf(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        )
+    }
 
     private var isLoaded = false
     private var doubleBackToExitPressedOnce = false
@@ -78,6 +98,7 @@ class MainActivity : AppCompatActivity() {
                     if (!isLoaded) loadWebView()
 
                 }
+
                 else -> {
                     showNoInternet()
                     snackbar.show()
@@ -97,9 +118,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setProgressDialogVisibility(visible: Boolean) {
-        if (visible){
+        if (visible) {
             loadingDialog.show()
-        }else{
+        } else {
             loadingDialog.dismiss()
             mainBinding.swipeRefresh.isRefreshing = false
         }
@@ -116,6 +137,18 @@ class MainActivity : AppCompatActivity() {
         gone(mainBinding.noInternet.noInternetRL)
         visible(mainBinding.webView)
         mainBinding.webView.loadUrl(webUrl)
+        mainBinding.webView.setDownloadListener { url, userAgent, contentDisposition, mimeType, contentLength ->
+
+            Log.d("Url", url.trim())
+            Log.d("userAgent", userAgent)
+            Log.d("contentDisposition", contentDisposition)
+            Log.d("mimeType", mimeType)
+            Log.d("contentLength", contentLength.toString())
+            if (checkMultiplePermission()) {
+                download(url.trim(),userAgent,contentDisposition,mimeType,contentLength)
+            }
+
+        }
         mainBinding.webView.webViewClient = object : WebViewClient() {
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 setProgressDialogVisibility(true)
@@ -171,15 +204,128 @@ class MainActivity : AppCompatActivity() {
             doubleBackToExitPressedOnce -> {
                 finish()
             }
+
             else -> {
                 doubleBackToExitPressedOnce = true
-                Toast.makeText(this,"Please Click Back Again to Exit",Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Please Click Back Again to Exit", Toast.LENGTH_LONG).show()
                 Handler(Looper.getMainLooper()).postDelayed(
                     {
-                    doubleBackToExitPressedOnce = false
-                    }
-                ,2000
+                        doubleBackToExitPressedOnce = false
+                    }, 2000
                 )
+            }
+        }
+    }
+
+    private fun download(
+        url: String,
+        userAgent: String,
+        contentDisposition: String,
+        mimeType: String,
+        contentLength: Long
+    ) {
+        val folder = File(
+            Environment.getExternalStorageDirectory().toString() + "/Download/Image"
+        )
+        if (!folder.exists()) {
+            folder.mkdirs()
+        }
+        Toast.makeText(this, "Download Started", Toast.LENGTH_SHORT).show()
+
+
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.setMimeType(mimeType)
+        val cookie = CookieManager.getInstance().getCookie(url)
+        request.addRequestHeader("cookie",cookie)
+        request.addRequestHeader("User-Agent",userAgent)
+        request.setAllowedNetworkTypes(
+            DownloadManager.Request.NETWORK_WIFI or
+                    DownloadManager.Request.NETWORK_MOBILE
+        )
+        val fileName = URLUtil.guessFileName(url, contentDisposition, mimeType)
+        request.setTitle(fileName)
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(
+            Environment.DIRECTORY_DOWNLOADS,
+            "Image/$fileName"
+        )
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        downloadManager.enqueue(request)
+
+    }
+
+
+    private fun checkMultiplePermission(): Boolean {
+        val listPermissionNeeded = arrayListOf<String>()
+        for (permission in multiplePermissionNameList) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                listPermissionNeeded.add(permission)
+            }
+        }
+        if (listPermissionNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                listPermissionNeeded.toTypedArray(),
+                multiplePermissionId
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == multiplePermissionId) {
+            if (grantResults.isNotEmpty()) {
+                var isGrant = true
+                for (element in grantResults) {
+                    if (element == PackageManager.PERMISSION_DENIED) {
+                        isGrant = false
+                    }
+                }
+                if (isGrant) {
+                    // here all permission granted successfully
+                    Toast.makeText(this,"all permission granted successfully",Toast.LENGTH_LONG).show()
+                } else {
+                    var someDenied = false
+                    for (permission in permissions) {
+                        if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                                this,
+                                permission
+                            )
+                        ) {
+                            if (ActivityCompat.checkSelfPermission(
+                                    this,
+                                    permission
+                                ) == PackageManager.PERMISSION_DENIED
+                            ) {
+                                someDenied = true
+                            }
+                        }
+                    }
+                    if (someDenied) {
+                        // here app Setting open because all permission is not granted
+                        // and permanent denied
+                        appSettingOpen(this)
+                    } else {
+                        // here warning permission show
+                        warningPermissionDialog(this) { _: DialogInterface, which: Int ->
+                            when (which) {
+                                DialogInterface.BUTTON_POSITIVE ->
+                                    checkMultiplePermission()
+                            }
+                        }
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/coding/meet/webviewtoapp/util.kt
+++ app/src/main/java/com/coding/meet/webviewtoapp/util.kt
@@ -1,6 +1,13 @@
 package com.coding.meet.webviewtoapp
 
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 enum class Status{
     Available,Unavailable
@@ -12,4 +19,26 @@ fun gone(view : View){
 
 fun visible(view : View){
     view.visibility = View.VISIBLE
+}
+
+
+fun appSettingOpen(context: Context){
+    Toast.makeText(
+        context,
+        "Go to Setting and Enable All Permission",
+        Toast.LENGTH_LONG
+    ).show()
+
+    val settingIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+    settingIntent.data = Uri.parse("package:${context.packageName}")
+    context.startActivity(settingIntent)
+}
+
+fun warningPermissionDialog(context: Context,listener : DialogInterface.OnClickListener){
+    MaterialAlertDialogBuilder(context)
+        .setMessage("All Permission are Required for this app")
+        .setCancelable(false)
+        .setPositiveButton("Ok",listener)
+        .create()
+        .show()
 }
